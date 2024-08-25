7f1534f2afdfb447aaa3232efdbae99ea8c90cd7
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2024-08-25 08:17:40 +0000

    Update 8 files
    - /app.py
    - /wiki/layout.html
    - /wiki/menu.html
    - /static/css/description.css
    - /static/css/design.css
    - /static/css/footer.css
    - /static/css/menu.css
    - /static/style.css

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template
-import time
 from os import path
 from pathlib import Path
-from flask_frozen import Freezer  # Update this line
 
-# Set up paths
-static_folder_path = path.abspath('./static')
-template_folder_path = path.abspath('./wiki')
-app = Flask(__name__, static_folder=static_folder_path, template_folder=template_folder_path)
+from flask import Flask, render_template
+from flask_frozen import Freezer
 
-# Flask-Frozen Configuration
+
+template_folder = path.abspath('./wiki')
+
+app = Flask(__name__, template_folder=template_folder)
+#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
@@ -17,32 +16,20 @@ freezer = Freezer(app)
 
 @app.cli.command()
 def freeze():
-    """Command to freeze the app"""
     freezer.freeze()
 
 @app.cli.command()
 def serve():
-    """Command to serve the frozen pages"""
     freezer.run()
-
-@app.after_request
-def add_header(response):
-    # Disable caching for development
-    response.cache_control.no_cache = True
-    response.cache_control.no_store = True
-    response.cache_control.max_age = 0
-    response.cache_control.public = False
-    return response
 
 @app.route('/')
 def home():
-    current_time = int(time.time())
-    return render_template('pages/home.html', current_time=current_time)
+    return render_template('pages/home.html')
 
 @app.route('/<page>')
 def pages(page):
-    current_time = int(time.time())
-    return render_template(f'pages/{page.lower()}.html', current_time=current_time)
+    return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
 
+# Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8080)
