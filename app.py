33c494df70f64d41fdcbbc730e09e6b07bd661de
Aditya Kumar <aditya.kr.agrawal1108@gmail.com>
2024-09-28 19:46:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from flask_frozen import Freezer
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
+app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
-#app.config['FREEZER_RELATIVE_URLS'] = True
+app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 freezer = Freezer(app)
 
